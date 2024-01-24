f6156ae68b66eff742b02c6b0f47c01108206bb4
MarciolimaDev <marclima998@gmail.com>
2024-01-24 17:30:08 -0500

    api teste version 11.0

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello, world!')
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/tokenbm")
+def read_root():
+    return {"id": "1", "name": "tokenbm", "token": "UNzQ4mDY-l7lFPjHdntg"}
